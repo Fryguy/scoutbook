4d03642eac239ec4146041e8e659ab91e5005f5f
Jason Frey <fryguy9@gmail.com>
2019-03-28 19:24:54 -0400

    Fix syntax error

--- scoutbook_checkbox_remover.js
+++ scoutbook_checkbox_remover.js
@@ -1,8 +1,8 @@
-function scoutbook_checkbox_remover() {
+(function scoutbook_checkbox_remover() {
   document.querySelectorAll('#report tr').forEach((row) => {
     var img = row.querySelector('img.checkboxIcon');
     if (img && (img.src.includes("checkboxapproved") || img.src.includes("checkboxawarded") || img.src.includes("checkboxemptygray"))) {
       row.style.display = 'none';
     }
   });
-}();
+})();
