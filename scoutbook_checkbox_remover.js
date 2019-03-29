ec53c6bc21cbc01b5eb3152ea4049b10b0b5703a
Jason Frey <fryguy9@gmail.com>
2019-03-28 21:31:44 -0400

    Check the entire row

--- scoutbook_checkbox_remover.js
+++ scoutbook_checkbox_remover.js
@@ -1,8 +1,13 @@
-(function scoutbook_checkbox_remover() {
+function scoutbook_checkbox_remover() {
   document.querySelectorAll('#report tr').forEach((row) => {
-    var img = row.querySelector('img.checkboxIcon');
-    if (img && (img.src.includes("checkboxapproved") || img.src.includes("checkboxawarded") || img.src.includes("checkboxemptygray"))) {
+    var rowImages = Array.from(row.querySelectorAll('img.checkboxIcon'));
+    var rowComplete = rowImages.length > 0 && rowImages.every((i) => {
+      return i.src.includes("checkboxapproved") || i.src.includes("checkboxawarded") || i.src.includes("checkboxemptygray")
+    });
+    if (rowComplete) {
       row.style.display = 'none';
     }
-  });
-})();
+  })
+}
+
+scoutbook_checkbox_remover();
