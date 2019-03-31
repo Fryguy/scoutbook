0888b66b81614bc3379c8e6c604db4fe0fbe4ec0
Jason Frey <fryguy9@gmail.com>
2019-03-31 13:59:51 -0400

    Update bookmarklet with a better link

--- report_diff_bookmarklet.js
+++ report_diff_bookmarklet.js
@@ -1,1 +1,1 @@
-javascript:(function(){s=document.createElement('script');s.type='text/javascript';s.src='https://cdn.jsdelivr.net/gh/Fryguy/scoutbook/report_diff.js';document.body.appendChild(s);})();
+javascript:(function(){s=document.createElement('script');s.type='text/javascript';s.src='https://fryguy.github.io/scoutbook/report_diff.js';document.body.appendChild(s);})();
